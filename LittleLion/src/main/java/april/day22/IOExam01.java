9137df7450918f6cb03d5802f3db4c5283f47210
CodeJesterND <dev.hyoseung@gmail.com>
2024-05-03 00:41:47 +0900

    IO Thread&ChatClientServer example study upload

--- LittleLion/src/main/java/april/day22/IOExam01.java
+++ LittleLion/src/main/java/april/day22/IOExam01.java
@@ -1,4 +1,4 @@
-package april.day22;
+package com.exam.day06;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -7,16 +7,14 @@ import java.io.IOException;
 public class IOExam01 {
     public static void main(String[] args) {
         //파일로 부터 읽어와서 콘솔에 출력하는 코드를 작성해보자.
-        try(FileInputStream fis = new FileInputStream("ioexam.txt");){
+        try(FileInputStream fis = new FileInputStream("gradlew.bat");){
             int i;
             int count=0;
-            /*
-            while((i = fis.read()) != -1){
-                count++;
-                System.out.print((char) i);
-            }
-             */
-            byte[] b = new byte[512];
+//            while((i = fis.read()) != -1){
+//                count++;
+//                System.out.print((char) i);
+//            }
+            byte[] b = new byte[1024];
             while((i = fis.read(b)) != -1){
                 count++;
                 String s = new String(b,0,i);
@@ -25,7 +23,7 @@ public class IOExam01 {
 
             System.out.println("총 "+count+"회 읽어옴.");
         }catch (FileNotFoundException e){
-            e.printStackTrace();
+//            e.printStackTrace();
             System.out.println("파일이 존재하지 않아요!!");
         }catch (IOException e){
             System.out.println(e);
